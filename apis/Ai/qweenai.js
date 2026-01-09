4de6d1ecbac8fada0ea7a3cdc641e2093d0e3b2c
x-emam <1emamabolela@gmail.com>
2026-01-10 00:55:02 +0200

    Implement qwenChat function for AI interactions

---
+++ apis/Ai/qweenai.js
@@ -0,0 +1,41 @@
+import setting from '../../default.js';
+
+const {
+  client
+} = setting;
+
+const qwenChat = async (q, options = {}) => {
+  const { 
+    image = null, 
+    video = null, 
+    audio = null, 
+    document = null, 
+    text2image = false, 
+    size = '1:1',
+    search = false, 
+    thinking = false 
+  } = options;
+
+  const body = {
+    q,
+    text2image,
+    size,
+    search,
+    thinking
+  };
+
+  if (image) body.image = image;
+  if (video) body.video = video;
+  if (audio) body.audio = audio;
+  if (document) body.document = document;
+
+  const res = await client('/home/sections/Ai/api/qwen-chat', {
+    method: 'POST',
+    data: body
+  });
+  
+  return res.answer;
+};
+
+export { qwenChat };
+export default qwenChat
